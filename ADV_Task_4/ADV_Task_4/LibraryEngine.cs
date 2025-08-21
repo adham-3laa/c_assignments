347e6e2d101340166c6cb8cba80dace379a41e26
adham-3la2 <adhamalaafouad_sd2024@fcis.bsu.edu.eg>
2025-08-21 23:53:56 +0300

    C44-G01-ADV04

---
+++ ADV_Task_4/ADV_Task_4/LibraryEngine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV_Task_4
+{
+    //a)	Create User Defined Delegate with the same signature of methods existed in Bookfunctions class.
+    internal delegate string BookFunctionDelegate(Book book);
+    internal class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> books, BookFunctionDelegate fptr)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine(fptr(book));
+            }
+                        
+        }
+
+        //b)	Use the Proper build in delegate. 
+
+        //public static void ProcessBooks(List<Book> books, Func<Book, string> fptr)
+        //{
+        //    foreach (var book in books)
+        //    {
+        //        Console.WriteLine(fptr(book));
+        //    }
+        //}
+    }
+}
---
+++ ADV_Task_4/ADV_Task_4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ADV_Task_4
+{
+    
+
+
+    internal class Program
+    {
+       
+       
+        private static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>
+            {
+                new Book("978-3-16-148410-0", "C# Programming",new[]{ "John Doe"},
+                    new DateTime(2020, 1, 1), 29.99m),
+                new Book("978-1-23-456789-7", "Advanced C#",
+                    new[] { "Alice Johnson" },
+                    new DateTime(2021, 5, 15), 39.99m)
+            };
+            //BookFunctionDelegate titleDelegate = BookFunctions.GetTitle;
+            //BookFunctionDelegate authorsDelegate = BookFunctions.GetAuthors;
+            //BookFunctionDelegate priceDelegate = BookFunctions.GetPrice;
+            //Console.Write("Book Titles:");
+            //LibraryEngine.ProcessBooks(books, titleDelegate);
+            //Console.Write("\nBook Authors:");
+            //LibraryEngine.ProcessBooks(books, authorsDelegate);
+            //Console.Write("\nBook Prices:");
+            //LibraryEngine.ProcessBooks(books, priceDelegate);
+            //c)	Anonymous Method(GetISBN).
+            LibraryEngine.ProcessBooks(books, delegate (Book book)
+            {
+                return book.ISBN;
+            });
+            //d)	Lambda Expression (GetPublicationDate).
+            LibraryEngine.ProcessBooks(books, book => book.PublicationDate.ToShortDateString());
+
+        }
+    }
+}
